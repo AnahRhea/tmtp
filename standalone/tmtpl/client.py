cf757b1244aa9bd5ca7a2cfbb176677950781ae3
Sacha Chua <sacha@sachachua.com>
2015-10-27 10:19:05 -0400

    Add the -m / --measurement command line argument
    This allows you to add or override client measurements from the
    command line, which is helpful for quickly generating variations.
    Ex: mkpattern ... -m length=20in output.svg
    
    - mkpattern: Parse the -m / --measurement argument. Add to documentation.
    - client.py: Refactor measurement-setting code into setMeasurement function.

--- standalone/tmtpl/client.py
+++ standalone/tmtpl/client.py
@@ -60,6 +60,44 @@ class Client(object):
         if filetype == 'json':
             self.__readJson__(filename)
 
+    def setMeasurement(self, key, val, conversion_factor=None):
+        keyparts = key.split('.')
+        if conversion_factor is None:
+            conversion_factor = self.__conversion__
+        # make sure the objects are created in the dotted 'path'
+        parent = self.data
+        for i in range (0, len(keyparts)-1):
+            oname = keyparts[i]
+            if oname not in dir(parent):
+                # object does not exist, create a new ClientData object within the parent
+                setattr(parent, oname, ClientData())
+                # Now, set the parent to be the object we just created
+                parent = getattr(parent, oname)
+            else:
+                # object exists - it better be a clientdata type and not something
+                # else. This can be caused by errors in the variable naming in the json file
+                parent = getattr(parent, oname)
+                if not isinstance(parent, ClientData):
+                    print "########################### ERROR: Malformed Client Data ###########################"
+                    print "\nThe variable named <", oname, "> appears both as an attribute and as a parent"
+                    print "Check the data file <", datafilename, ">"
+                    print "\n####################################################################################"
+                    raise ValueError
+
+        # now, we have all the containing objects in place
+        # get the rightmost part of the dotted variable, and add it
+        attrname = keyparts[-1]
+        # Create attribute based on the type in the json data
+        ty = val['type']
+        if ty == 'float':
+            setattr(parent, attrname, float(val['value']) * conversion_factor)
+        elif ty == 'string':
+            setattr(parent, attrname, val['value'])
+        elif ty == 'int':
+            setattr(parent, attrname, int(val['value']))
+        else:
+            raise ValueError('Unknown type ' + ty + 'in client data')
+                
     def __readJson__(self, datafilename):
         self.info={}
 
@@ -82,47 +120,13 @@ class Client(object):
             raise
 
         #
-        # read all these and then create a heirarchy of objects and
+        # read all these and then create a hierarchy of objects and
         # attributes, based on the 'dotted path' notation.
         #
 
         # read everything into attributes
         for key, val in self.client.items():
-            keyparts = key.split('.')
-
-            # make sure the objects are created in the dotted 'path'
-            parent = self.data
-            for i in range (0, len(keyparts)-1):
-                oname = keyparts[i]
-                if oname not in dir(parent):
-                    # object does not exist, create a new ClientData object within the parent
-                    setattr(parent, oname, ClientData())
-                    # Now, set the parent to be the object we just created
-                    parent = getattr(parent, oname)
-                else:
-                    # object exists - it better be a clientdata type and not something
-                    # else. This can be caused by errors in the variable naming in the json file
-                    parent = getattr(parent, oname)
-                    if not isinstance(parent, ClientData):
-                        print "########################### ERROR: Malformed Client Data ###########################"
-                        print "\nThe valiable named <", oname, "> appears both as an attribute and as a parent"
-                        print "Check the Data file <", datafilename, ">"
-                        print "\n####################################################################################"
-                        raise ValueError
-
-            # now, we have all the containing objects in place
-            # get the rightmost part of the dotted variable, and add it
-            attrname = keyparts[-1]
-            # Create attribute based on the type in the json data
-            ty = val['type']
-            if ty == 'float':
-                setattr(parent, attrname, float(val['value']) * self.__conversion__)
-            elif ty == 'string':
-                setattr(parent, attrname, val['value'])
-            elif ty == 'int':
-                setattr(parent, attrname, int(val['value']))
-            else:
-                raise ValueError('Unknown type ' + ty + 'in client data')
+            self.setMeasurement(key, val)
         return
 
     def __dump__(self, obj, parent = '', parentstring = '', outtxt = []):
